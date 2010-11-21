afa61aa5e5a300435b6ead752690c4c760d84702
Tim Butram <atrus6@gmail.com>
2010-11-20 21:22:55 -0500

    Added JavaDocs to Event.

--- src/event/Event.java
+++ src/event/Event.java
@@ -5,11 +5,18 @@ public class Event
 
 	protected Object source;
 	
+	/**
+	 * Blank Event, with a null Source.
+	 */
 	public Event()
 	{
 		source = null;
 	}
 	
+	/**
+	 * Creates an Event with a given source.
+	 * @param source - The source of the Event.
+	 */
 	public Event(Object source)
 	{
 		this.source = source;
@@ -24,6 +31,10 @@ public class Event
 		return source;
 	}
 	
+	/**
+	 * Sets the souce of the Event.
+	 * @param source - The new source of the Event.
+	 */
 	public void setSource(Object source)
 	{
 		this.source = source;
