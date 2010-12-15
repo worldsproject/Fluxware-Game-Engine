62cdc709867b5c5443013d4c07493db1f10580bc
Tim Butram <atrus6@gmail.com>
2010-12-15 18:36:19 -0500

    Added JavaDocs and comments.

--- src/gui/DisplayPanel.java
+++ src/gui/DisplayPanel.java
@@ -25,16 +25,18 @@ import sprites.Sprite;
  */
 public class DisplayPanel extends JPanel 
 {
-	private Room r;
-	private HUD h = null;
+	private Room r; //The displayed room.
+	private HUD h = null; //Optional HUD
 
-	private Dimension resolution;
-	private Dimension size;
-	private int spacing = 1;
-	
+	private Dimension resolution; //The resolution the window is showing.
+	private Dimension size; //The pixel size of the Room.
+	private int spacing = 1; //Each cell.
+
+	//The X and Y coordinates of the ViewPort.
 	private int viewX = 0;
 	private int viewY = 0;
 
+	//For DoubleBuffering.
 	Graphics buf;
 	BufferedImage offscreen;
 
@@ -45,20 +47,21 @@ public class DisplayPanel extends JPanel
 	 */
 	public DisplayPanel(Room r, Dimension res)
 	{
-		//Sets so the the Display Driver only  **Feb 1, 2010  WTF does this comment mean?
+		//Sets the space to the TiledRoom cellsize.
 		if(r instanceof TiledRoom)
 		{
 			spacing = ((TiledRoom)r).getCellSize();
 		}
 
-		resolution = res;
-		
+		resolution = res; //How big the window should be.
+
 		//Setting the size of the DisplayPanel.
 		int width = r.getWidth();
 		int length = r.getHeight();
 
-		size = new Dimension((spacing * width), (spacing * length));
+		size = new Dimension((spacing * width), (spacing * length)); //Pixel size of the room.
 
+		//Forces the JPanel to be the correct size.
 		this.setMinimumSize(res);
 		this.setPreferredSize(res);
 		this.setMaximumSize(res);
@@ -72,50 +75,76 @@ public class DisplayPanel extends JPanel
 		//Setting the room up.
 		this.r = r;
 	}
-	
+
+	/**
+	 * Changes the current displayed Room to the given Room.
+	 * @param r - The new Room to be displayed.
+	 */
 	public void changeRoom(Room r)
 	{
-		this.r = r;
+		this.r = r; //Changes the room.
 	}
 
+	/**
+	 * Adds a HUD to the display.
+	 * @param h - The HUD to be displayed.
+	 */
 	public void addHUD(HUD h)
 	{
-		this.h = h;
+		this.h = h; //Sets the HUD.
 		h.setWidth(r.getWidth());
 		h.setHeight(h.getHeight());
 	}
 
+	/**
+	 * This calls every Sprites update method with the total time since the game started, and time since the last frame refresh.
+	 * @param time
+	 * @param totalTime
+	 */
 	public void updateSprites(long time, long totalTime)
 	{
-		LinkedList<Sprite> l = r.getSprites();
+		LinkedList<Sprite> l = r.getSprites();  //Iterate through each Sprite and call its update method.
 		for(Sprite s : l)
 		{
 			s.update(time, totalTime);
 		}
 	}
-	
+
+	/**
+	 * Scroll the ViewScreen by the amount given. This is relative to it's current location.
+	 * Thus, if the current Viewport is at (100,100) and this method is called with (10, 5) the
+	 * Viewports X and Y coordinates will be (110, 105).
+	 * @param x - Amount the Viewports X coordinate will change by.
+	 * @param y - Amount the Viewports Y coordinate will change by.
+	 */
 	public void scrollViewscreen(int x, int y)
 	{
 		viewX += x;
 		viewY += y;
 	}
-	
+
+	/**
+	 * This moves the Viewscreen relative to a Sprite. The Viewscreen will be centered on
+	 * the supplied Sprite.
+	 * @param s - The Sprite the Viewscreen is to be centered upon.
+	 */
 	public void scrollRelativeTo(Sprite s)
 	{
-		if(s == null)
+		if(s == null) //If the Sprite is null, don't bother changing anything.
 		{
 			return;
 		}
-		
+
 		//First we need to calculate the center.
 		int width = s.getWidth();
 		int height = s.getHeight();
 		int vpWidth = (int) resolution.getWidth();
 		int vpHeight = (int) resolution.getHeight();
-		
+
 		int centerHeight = (vpHeight >> 1) - height;
 		int centerWidth = (vpWidth >> 1) - width;
-		
+
+		//Then we set the viewports X and Y to the calculated point.
 		viewY = s.getY() - centerHeight;
 		viewX = s.getX() - centerWidth;
 	}
@@ -126,44 +155,29 @@ public class DisplayPanel extends JPanel
 	@Override
 	public void paintComponent(Graphics g) 
 	{
-		buf = offscreen.getGraphics();
+		buf = offscreen.getGraphics(); //Get the offscreen graphics.
 
-		LinkedList<Sprite> l = r.getSprites();
+		LinkedList<Sprite> l = r.getSprites(); //Get the list of Sprites.
 
 		buf.setColor(Color.GRAY);  //General background of the screen.
 
-		buf.fillRect(0, 0, (int)size.getWidth(), (int)size.getHeight());
+		buf.fillRect(0, 0, (int)size.getWidth(), (int)size.getHeight()); //Fill everything with Gray.
 
-		buf.setColor(Color.BLACK);
+		buf.setColor(Color.BLACK); //Fill the viewscreen with Black.
 		buf.fillRect(viewX * -1, viewY * -1, r.getWidth(), r.getHeight());
 
 		buf.setColor(Color.WHITE);
 
-		for(Sprite temp: l)
+		for(Sprite temp: l) //Iterate through each sprite and draw it.
 		{	
-//			if(r.getWidth() >= resolution.getWidth() && r.getHeight() >= resolution.getHeight())
-//			{
-				buf.drawImage(temp.print(), (temp.getX() * spacing) - viewX, (temp.getY() * spacing) - viewY, null);
-//			}
-//			else if(r.getWidth() > resolution.getWidth())
-//			{
-//				buf.drawImage(temp.print(), (temp.getX() * spacing) - viewX, (temp.getY() * spacing) - viewY + (r.getHeight() >> 1), null);
-//			}
-//			else if(r.getHeight() > resolution.getHeight())
-//			{
-//				buf.drawImage(temp.print(), (temp.getX() * spacing) - viewX + (r.getWidth() >> 1), (temp.getY() * spacing) - viewY, null);
-//			}
-//			else
-//			{
-//				buf.drawImage(temp.print(), (temp.getX() * spacing) - viewX + (r.getWidth() >> 1), (temp.getY() * spacing) - viewY + (r.getHeight() >> 1), null);
-//			}	
+			buf.drawImage(temp.print(), (temp.getX() * spacing) - viewX, (temp.getY() * spacing) - viewY, null);	
 		}
 
 		if(h != null && h.isVisible()) //This deals with the display of the HUD
 		{
 			HashMap<Integer, Sprite> hash = h.getSprites();
 			Set<Integer> set = h.getSprites().keySet();
-			
+
 			for(Integer i : set)
 			{
 				Sprite s = hash.get(i);
