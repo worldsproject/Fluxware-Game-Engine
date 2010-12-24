b5f5ada42b33ae67cdaa851f85ee450a98b2f48f
Tim Butram <atrus6@gmail.com>
2010-12-23 20:24:09 -0500

    More commenting.

--- src/listener/MultiKeyListener.java
+++ src/listener/MultiKeyListener.java
@@ -4,6 +4,11 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.HashMap;
 
+/**
+ * A slightly better implementation of the Java KeyListeners that allows for easier handling of
+ * Keystrokes.
+ *
+ */
 public class MultiKeyListener implements KeyListener 
 {
 	private HashMap<Integer, Boolean> keys = new HashMap<Integer, Boolean>();
