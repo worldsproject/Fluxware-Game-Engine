c00a784db01e2587cf89c91d449a65cf0c8e7c18
Tim Butram <atrus6@gmail.com>
2010-12-25 12:41:42 -0500

    Removed LevelReader as LevelConversions is the new and improved version.

--- src/level/TiledRoom.java
+++ src/level/TiledRoom.java
@@ -1,13 +1,10 @@
 package level;
 import java.awt.event.KeyListener;
-import java.io.File;
-import java.util.HashMap;
 import java.util.LinkedList;
 
 import listener.bounding.Bounding;
 import listener.bounding.TiledBoundingBox;
 import sprites.Sprite;
-import util.LevelReader;
 import util.Point2D;
 
 
@@ -37,29 +34,6 @@ public class TiledRoom extends Room
 {
 	//This is a default size of the cells.
 	private int cellSize = 30;
-
-	/**
-	 * This creates a Tiled Room based off of a Level created either by hand or
-	 * by the Fluxware Level Editor.
-	 * 
-	 * @param f - The File containing the location of the Level File.
-	 * @param ex - A Hashmap containing the Serial and example Sprites.
-	 */
-	public TiledRoom(File f, HashMap<Integer, Sprite> ex)
-	{
-		LevelReader reader = new LevelReader(f, ex);
-
-		LinkedList<Sprite> temp = reader.getSprites();
-		
-		for(Sprite s : temp)
-		{
-			addSprite(s);
-		}
-
-		this.width = reader.getWidth();
-		this.height = reader.getHeight();
-		this.layers = reader.getLayers();
-	}
 	
 	/**
 	 * Creates and empty room with the given dimensions.
