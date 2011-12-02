710e39319eca7b582b131de74f58fd07ee1f79b9
Tim Butram <atrus6@gmail.com>
2011-12-02 14:51:18 -0500

    Removed all old test items. This should be implemented in a seperate area.

--- src/gui/Game.java
+++ src/gui/Game.java
@@ -23,36 +23,35 @@ import error.CrashReport;
  * @author Fluxware
  *
  */
-public class Game extends JFrame implements KeyListener, MouseListener
+public class Game
 {
-	private GraphicsDevice device = null;
-	private Room room;
-	protected DisplayPanel dp;
-	private RunThread rt;
-
-	private boolean fullscreen = false;
-
-	protected JPanel root = null;
-
-	private boolean showMenu = false;
-	private Menu menu = null;
+	//Sets the default size of the window that the game is displayed in.
+	private Dimension size = new Dimension(800, 600);
 	
-	private Dimension resolution = new Dimension(1024, 768);
+	//Sets whether the window will display in fullscreen. Default false.
+	private boolean fullscreen = false;
+	
+	//The current room that the game is displaying.
+	private Room room = null;
 
-	public Game(){}
-
+	/**
+	 * Creates a Game with the given Room. Uses defaults of 800x600 and not in fullscreen.
+	 * @param room
+	 */
+	public Game(Room room)
+	{
+		this.room = room;
+	}
+	
 	/**
 	 * Creates a new Game based on the given room and if it should be fullscreen or not.
 	 * @param room - The room to be displayed.
 	 * @param fullscreen - True to enable fullscreen. 
 	 */
 	public Game(Room room, boolean fullscreen)
-	{
-		super("Fluxware Game Engine");
-		
+	{	
 		this.room = room;
-		
-		construct(fullscreen);
+		this.fullscreen = fullscreen;
 	}
 	
 	/**
@@ -63,14 +62,7 @@ public class Game extends JFrame implements KeyListener, MouseListener
 	 */
 	public Game(Room room, boolean fullscreen, Dimension size)
 	{
-		this.room = room;
-		
-		if(size != null)
-		{
-			resolution = size;
-		}
-		
-		construct(fullscreen);
+
 	}
 	
 	/**
@@ -82,23 +74,7 @@ public class Game extends JFrame implements KeyListener, MouseListener
 	 */
 	public Game(Room room, boolean fullscreen, Dimension size, String title)
 	{
-		this.room = room;
-		
-		if(title != null)
-		{
-			this.setTitle(title);
-		}
-		else
-		{
-			this.setTitle("Fluxware Game Engine");
-		}
-		
-		if(size != null)
-		{
-			resolution = size;
-		}
-		
-		construct(fullscreen);
+
 	}
 
 	/**
@@ -109,14 +85,7 @@ public class Game extends JFrame implements KeyListener, MouseListener
 	 */
 	public Game(Room room, boolean fullscreen, String title)
 	{
-		this.room = room;
-		
-		if(title != null)
-			this.setTitle(title);
-		else
-			this.setTitle("Fluxware Game Engine");
 
-		construct(fullscreen);
 	}
 	
 	/*
@@ -124,16 +93,7 @@ public class Game extends JFrame implements KeyListener, MouseListener
 	 */
 	private void construct(boolean fullscreen)
 	{
-		this.fullscreen = fullscreen;
 		
-		this.addKeyListener(this);
-		this.addMouseListener(this);
-		
-		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-
-		setRoom(room);
-		setupGUI();
-		run();
 	}
 
 	/**
@@ -143,65 +103,7 @@ public class Game extends JFrame implements KeyListener, MouseListener
 	 */
 	protected void setupGUI()
 	{
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  //Makes sure it fully exits when you click the 'X'
 
-		setupDP(room);  //Sets up the Display Panel with the first room.
-
-		root = (JPanel)this.getContentPane();  //Gets the content pane, and sets it up so that it is always centered.
-		root.setLayout(new GridBagLayout());
-
-		root.add(dp, new GridBagConstraints());
-
-		//This is here to determine the fullscreen status
-		if(fullscreen == false)//Keep it as a window.
-		{
-			this.setSize(resolution);
-
-			this.setLocationRelativeTo(null);
-		}
-		else //Fullscreen it!
-		{
-			this.setUndecorated(true);
-			this.setResizable(false);
-
-			GraphicsDevice[] gda = GraphicsEnvironment.getLocalGraphicsEnvironment().getScreenDevices();
-			device = gda[0];
-			device.setFullScreenWindow(this);
-		}
-
-		//Makes it visible and adds a keylistener.
-		this.setVisible(true);
-		this.addKeyListener(room);
-		
-		for(KeyListener k: room.getKeyListeners())
-		{
-			this.addKeyListener(k);
-		}
-	}
-
-	/*
-	 * Changes the room.
-	 */
-	protected void setupDP(Room room)
-	{
-		if(dp != null)
-		{
-			dp.changeRoom(room);
-		}
-		else
-		{
-			dp = new DisplayPanel(room, resolution);
-		}
-	}
-
-	/*
-	 * Creates a new thread and runs it.  This thread handles the updates.
-	 */
-	protected void run()
-	{
-		rt = new RunThread(this);
-		rt.setPriority(Thread.MAX_PRIORITY);
-		rt.start();
 	}
 
 	/**
@@ -210,10 +112,7 @@ public class Game extends JFrame implements KeyListener, MouseListener
 	 */
 	public void setRoom(Room room)
 	{
-		this.room = null;
-
-		this.room = room;
-		setupDP(room);
+		//TODO
 	}
 	
 	/**
@@ -221,7 +120,7 @@ public class Game extends JFrame implements KeyListener, MouseListener
 	 */
 	public Room getRoom()
 	{
-		return room;
+		return null; //TODO
 	}
 	
 	/**
@@ -231,7 +130,7 @@ public class Game extends JFrame implements KeyListener, MouseListener
 	 */
 	public void update(long totalTime, long elapsedTime)
 	{
-
+		//TODO
 	}
 	
 	/**
@@ -241,7 +140,7 @@ public class Game extends JFrame implements KeyListener, MouseListener
 	 */
 	public void scrollViewscreen(int x, int y)
 	{
-		dp.scrollViewscreen(x, y);
+		//TODO
 	}
 	
 	/**
@@ -250,108 +149,6 @@ public class Game extends JFrame implements KeyListener, MouseListener
 	 */
 	public void scrollViewscreenRelativeTo(Sprite s)
 	{
-		dp.scrollRelativeTo(s);
-	}
-
-	/*
-	 * This private class creates a new thread and handles the screen refreshes.
-	 */
-	private class RunThread extends Thread 
-	{
-		private long time = System.currentTimeMillis();
-		private Game g = null;
-		RunThread(Game g)
-		{
-			this.g = g;
-		}
-
-
-		public void run()
-		{	
-			try
-			{
-				long currentTime = System.currentTimeMillis();
-				long totalTime = 0;
-				while(true)
-				{
-					long lastTime = currentTime;
-					currentTime = System.currentTimeMillis();
-
-					time = currentTime - lastTime;
-					totalTime += time;
-
-					root.repaint();
-
-					dp.updateSprites(time, totalTime);
-					g.update(time, totalTime);
-					dp.repaint();
-
-					try 
-					{
-						long sleepTime = System.currentTimeMillis() - currentTime;
-
-						Thread.sleep(Math.max(17 - sleepTime, 0));
-					}
-					catch (InterruptedException e) 
-					{
-						e.printStackTrace();
-					}
-				}
-			}
-			catch(Exception e)
-			{
-				new CrashReport(e);
-			}
-
-		}
-		
-	}
-
-	@Override
-	public void keyPressed(KeyEvent e) 
-	{
-
-	}
-
-	@Override
-	public void keyReleased(KeyEvent arg0) {
-		// TODO Auto-generated method stub LACE
-
-	}
-
-	@Override
-	public void keyTyped(KeyEvent arg0) {
-		// TODO Auto-generated method stub LACE
-
-	}
-
-	@Override
-	public void mouseClicked(MouseEvent arg0) {
-		// TODO Auto-generated method stub LACE
-
-	}
-
-	@Override
-	public void mouseEntered(MouseEvent arg0) {
-		// TODO Auto-generated method stub LACE
-
-	}
-
-	@Override
-	public void mouseExited(MouseEvent arg0) {
-		// TODO Auto-generated method stub LACE
-
-	}
-
-	@Override
-	public void mousePressed(MouseEvent arg0) {
-		// TODO Auto-generated method stub LACE
-
-	}
-
-	@Override
-	public void mouseReleased(MouseEvent arg0) {
-		// TODO Auto-generated method stub LACE 
-
+		//TODO
 	}
 }
