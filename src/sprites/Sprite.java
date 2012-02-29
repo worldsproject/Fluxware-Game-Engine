651cdd7b0d30c66993290702247533d1845e24f9
Tim Butram <atrus6@gmail.com>
2012-02-29 18:38:31 -0500

    In the middle of telling the Sprite how to draw itself correctly.

--- src/sprites/Sprite.java
+++ src/sprites/Sprite.java
@@ -195,8 +195,36 @@ public class Sprite implements Serializable
 		int ty = (int)location.y;
 		glTranslatef(tx, ty, location.layer);
 		
-//		float height = imageData.getTexture().getHeight();
-//		float width = imageData.getTexture().getWidth();
+		float texture_X = ((float)which_column/(float)columns);
+		float texture_Y = ((float)which_row/(float)rows);
+		float texture_XplusWidth = ((float)(which_column+wide)/(float)columns);
+		float texture_YplusHeight = ((float)(which_row+tall)/(float)rows);
+		
+		glBegin(GL_QUADS);
+		{
+			glTexCoord2f(texture_X, texture_Y);
+			glVertex2f(0, 0);
+			
+			glTexCoord2f(texture_X, texture_YplusHeight);
+			glVertex2f(0, getHeight());
+			
+			glTexCoord2f(texture_XplusWidth, texture_YplusHeight);
+			glVertex2f(getWidth(), getHeight());
+			
+			glTexCoord2f(texture_XplusWidth, texture_Y);
+			glVertex2f(getWidth(), 0);
+		}
+		glEnd();
+		glPopMatrix();
+	}
+	
+	private void drawTiled()
+	{
+		glPushMatrix();
+		imageData.getTexture().bind();
+		int tx = (int)location.x;
+		int ty = (int)location.y;
+		glTranslatef(tx, ty, location.layer);
 		
 		float texture_X = ((float)which_column/(float)columns);
 		float texture_Y = ((float)which_row/(float)rows);
